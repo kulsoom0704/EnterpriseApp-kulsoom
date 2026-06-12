1be78c0f8b593244ea50635eea7fd0e066f273cd
kulsoom0704 <kulsoomhifazat2@gmail.com>
2026-06-12 14:30:39 +0200

    added more events as asked in the assignment.

--- src/main/java/com/example/demo/service/DataLoader.java
+++ src/main/java/com/example/demo/service/DataLoader.java
@@ -49,10 +49,58 @@ import java.time.LocalDateTime;
                 e4.setEmailContactPerson("seniors@hopengo.org");
                 e4.setTime(LocalDateTime.of(2026,7,25,11,0));
 
+                Event e5 = new Event();
+                e5.setTitle("Charity Fundraiser");
+                e5.setOrganization("Hope NGO");
+                e5.setDescription("Fundraising event for local community projects.");
+                e5.setEmailContactPerson("fundraising@hopengo.org");
+                e5.setTime(LocalDateTime.of(2026, 8, 10, 14, 0));
+
+                Event e6 = new Event();
+                e6.setTitle("Tree Plantation Drive");
+                e6.setOrganization("Green Earth");
+                e6.setDescription("Planting trees to promote sustainability.");
+                e6.setEmailContactPerson("trees@greenearth.org");
+                e6.setTime(LocalDateTime.of(2026, 8, 15, 9, 0));
+
+                Event e7 = new Event();
+                e7.setTitle("Blood Donation Camp");
+                e7.setOrganization("Red Care");
+                e7.setDescription("Community blood donation event.");
+                e7.setEmailContactPerson("blood@redcare.org");
+                e7.setTime(LocalDateTime.of(2026, 8, 20, 10, 0));
+
+                Event e8 = new Event();
+                e8.setTitle("Women's Empowerment Workshop");
+                e8.setOrganization("Empower Together");
+                e8.setDescription("Skills and leadership training.");
+                e8.setEmailContactPerson("women@empower.org");
+                e8.setTime(LocalDateTime.of(2026, 8, 25, 13, 0));
+
+                Event e9 = new Event();
+                e9.setTitle("Youth Leadership Camp");
+                e9.setOrganization("Future Leaders");
+                e9.setDescription("Leadership activities for young people.");
+                e9.setEmailContactPerson("youth@futureleaders.org");
+                e9.setTime(LocalDateTime.of(2026, 9, 1, 11, 0));
+
+                Event e10 = new Event();
+                e10.setTitle("Community Health Check");
+                e10.setOrganization("Health First");
+                e10.setDescription("Free health screenings for residents.");
+                e10.setEmailContactPerson("health@healthfirst.org");
+                e10.setTime(LocalDateTime.of(2026, 9, 5, 10, 0));
+
                 eventRepository.save(e1);
                 eventRepository.save(e2);
                 eventRepository.save(e3);
                 eventRepository.save(e4);
+                eventRepository.save(e5);
+                eventRepository.save(e6);
+                eventRepository.save(e7);
+                eventRepository.save(e8);
+                eventRepository.save(e9);
+                eventRepository.save(e10);
             }
         }
     }
